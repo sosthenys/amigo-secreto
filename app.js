54c88b1933bc2d2fd3dd1df0baaad091a7a3ec76
sosthenys <sosthenysfl@hotmail.com>
2025-08-07 12:05:26 -0300

    criação da estrutura das funções mundaças em variaveis

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
 let conteLista = listaAmigos.length
-let amigoSecretoEscolhido = ""
+let amigoEscolhido = ""
 let nomeAmigo = ""
+
+
+function    validarNome() {
+
+ nomeAmigo = document.querySelector('input-name').value ;
+
+if nomeAmigo = "" {
+
+    alert("Digite um nome e clique no botão Adicionar!");
+    break
+
+}
+
+
+function limparCampo() {
+
+    nomeAmigo = '';
+
+}
+
+}
+
+function    adicionarAmigo() {
+
+    validarNome()
+    
+    listaAmigos.push(nomeAmigo)
+
+    limparCampo()
+}
+
+function    sortearAmigo() {
+
+    amigoEscolhido = parseInt(Math.random() * conteLista + 1)
+
+
+}
+
+function    exibirListaAmigos(){
+
+    
+
+}
