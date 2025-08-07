b70b45755b1b79a3f6b61da7abc592cd3a0b3097
sosthenys <sosthenysfl@hotmail.com>
2025-08-07 10:45:54 -0300

    variaveis adicionadas

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+let conteLista = listaAmigos.length
+let amigoSecretoEscolhido = ""
+let nomeAmigo = ""
